d8a3e969aa8223c6a5f3c01507806d54c4cad7f1
KJT <kevin.jeanot@gmail.com>
2024-07-03 12:08:07 +0200

    Wishlist OK

--- assets/wish.js
+++ assets/wish.js
@@ -1,50 +1,103 @@
 const addToWishlistButton = document.querySelector('#wish');
+
+let product;
+
+if (addToWishlistButton) {
+
 const productId = addToWishlistButton.dataset.product;
-const wishlist= JSON.parse(localStorage.getItem("wishlist")) ?? [];
+const productTitle = addToWishlistButton.dataset.productTitle;
+const productUrl = addToWishlistButton.dataset.productUrl;
+const productImage = addToWishlistButton.dataset.productImage;
 
-console.log(wishlist);
+product = {
+  id: productId,
+  title: productTitle,
+  url: productUrl,
+  image: productImage
+};
 
+}
 
-
+const wishlistContainer = document.querySelector('.wishlist-container');
 
 const wish = {
 
   init: function() {
 
-    addToWishlistButton.addEventListener('click', wish.handleClick);
-
-    wish.isInWishlist(productId);
+    if (addToWishlistButton) {
+      addToWishlistButton.addEventListener('click', wish.handleClick);
+      wish.isInWishlist(product.id);
+    }
+    
+    if(wishlistContainer) {
+      wish.displayWishlist();
+    }
+    console.log(wishlistContainer);
    
   },
 
-  handleClick: function(evt) {
+  list: JSON.parse(localStorage.getItem("wishlist")) ?? [],
 
-    let updatedWishlist = [];
-    console.log(productId);
+  handleClick: function(evt) {
     
-    if (wish.isInWishlist(productId)) {
-      console.log('in wishlist');
+    if (wish.isInWishlist(product.id)) {
+      
       addToWishlistButton.classList.remove('in-wishlist');
-      updatedWishlist = wishlist.map(el => el != productId ? el : '');
-    } else {
-      console.log('not in wishlist');
-      addToWishlistButton.classList.add('in-wishlist');
-      updatedWishlist.push(productId);
+
+      wish.list = wish.list.filter(el => el.id !== product.id);
     }
 
-    localStorage.setItem("wishlist", JSON.stringify(updatedWishlist));
-    wish.isInWishlist(productId);
+    else {
+      wish.list.push(product);
+    }
+
+    wish.list ? localStorage.setItem("wishlist", JSON.stringify(wish.list)) : [];
+    wish.isInWishlist(product.id);
   
   },
 
-  isInWishlist: function(id) {
-    if (!wishlist.includes(id)) {
-      return false;
-    }
-    return true;
-  }
+  isInWishlist: function(productId) {
+
+    let result = false;
+
+    wish.list.forEach(el => {
+      if(el.id === productId) {
+        addToWishlistButton.classList.add('in-wishlist');
+        result = true;
+      }
+    });
+    
+    return result;
+  },
+
+  displayWishlist: function() {
+
+      wish.list.forEach(el => {
+
+      const product = document.createElement('div');
+
+      const productUrlNode = document.createElement('a');
+      productUrlNode.href = el.url;
+
+      const productImageNode = document.createElement('img');
+      productImageNode.src = el.image;
+
+      const productTitleNode = document.createElement('h3');
+      productTitleNode.textContent = el.title;
+
+      product.classList.add('product');
+      product.appendChild(productUrlNode);
+
+      productUrlNode.appendChild(productImageNode);
+      productUrlNode.appendChild(productTitleNode);
+
+      wishlistContainer.appendChild(product);
+    });
+
+
+
+  },
 
 }
 
 wish.init();
-wish.isInWishlist(productId);
